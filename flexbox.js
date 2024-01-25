13fee8aca9181d07b37523659d82f64bfe37f87a
hernan <hernansevenpor@gmail.com>
2024-01-25 00:13:31 -0300

    probandocosas

---
+++ flexbox.js
@@ -0,0 +1,16 @@
+var nombre ="hernan"; 
+var nombre2 = "hernan";
+var numero1 = 123445;
+var numerodecimal= 11.33;
+var numero2 = 11245;
+var numero3 = 14523;
+
+var booleano =true;
+var booleano2 = false
+var indefinido =undefined
+
+console.log (nombre); 
+console.log (nombre2);
+console.log (numero1);
+console.log (numerodecimal);
+console.log("el valor indefinido",indefinido);
